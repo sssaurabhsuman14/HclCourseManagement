9e867719649cd2dee0ee52725f6dfba931980210
saurabh suman <sssaurabhsuman@gmail.com>
2019-07-27 14:20:56 +0530

    service added

--- src/main/java/com/management/course/service/CourseService.java
+++ src/main/java/com/management/course/service/CourseService.java
@@ -1,8 +1,17 @@
 package com.management.course.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.management.course.model.CourseModel;
 
 @Service
 public interface CourseService {
 
+	public CourseModel createCourse(CourseModel model);
+	public CourseModel getCourse(Long id);
+	public List<CourseModel> getAll(Long id);
+	public CourseModel updateCourse(CourseModel model);
+	public CourseModel deleteCourse(CourseModel model);
 }
