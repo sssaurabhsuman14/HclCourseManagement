a052e41685e564a69ff5b2f28242aeaa93dcefcd
saurabh suman <sssaurabhsuman@gmail.com>
2019-07-27 14:34:04 +0530

    added models

---
+++ src/main/java/com/management/course/model/CourseRequestModel.java
@@ -0,0 +1,12 @@
+package com.management.course.model;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseRequestModel implements Serializable{
+
+	private static final long serialVersionUID = 468325764667369918L;
+
+}
