53d61023b9d939740c9d1cc80dff92b8204cb5ab
saurabh suman <sssaurabhsuman@gmail.com>
2019-07-27 14:33:18 +0530

    resolve

--- src/main/java/com/management/course/controller/TrainerController.java
+++ src/main/java/com/management/course/controller/TrainerController.java
@@ -1,12 +1,28 @@
 package com.management.course.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.management.course.model.CourseRequestModel;
+import com.management.course.service.CourseRequestService;
+
 @RestController
 @RequestMapping("/trainer")
-public class TrainerController 
-{
+
+public class TrainerController {
 	
+	@Autowired
+	CourseRequestService courseRequestService;
+	
+	@PostMapping
+	public CourseRequestModel trainerAction() {
+		
+		return null;
+		
+	}
 
 }
