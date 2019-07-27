b4e3c34d7b3066b9400db96a40704a79f3fdaa84
saurabh suman <sssaurabhsuman@gmail.com>
2019-07-27 14:19:37 +0530

    added controllers and model

---
+++ src/main/java/com/management/course/model/CourseModel.java
@@ -0,0 +1,12 @@
+package com.management.course.model;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseModel implements Serializable {
+
+	private static final long serialVersionUID = 5342982612373289412L;
+
+}
